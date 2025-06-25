Fix order of multiplication in silhouette score calculation for correct scaling

silhouette.py
@@ -95,4 +95,4 @@ def silhouette(Q: np.ndarray, y: np.ndarray, factor_h: float, factor_k: int) -> 
     s[mask_denom_ne_zero] = (b[mask_denom_ne_zero] - a[mask_denom_ne_zero]) / denominator[mask_denom_ne_zero]
 
     # The final score is the mean over all samples.
-    return float(np.mean(s)) * factor_h * factor_k
+    return float(np.mean(s)) * factor_k * factor_h